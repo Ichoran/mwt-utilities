293204f9ef15b222f36c6960d9f3595db49d9627
Rex Kerr <ichoran@gmail.com>
2019-01-16 16:15:03 -0800

    Added lots of code to deal with single entries in a summary file.

--- mwt_utilities/src/Summary.scala
+++ mwt_utilities/src/Summary.scala
@@ -6,7 +6,7 @@ import java.nio.file.attribute.FileTime
 import java.time._
 import java.util.zip._
 
-import scala.collection.JavaConverters._
+import java.lang.{ StringBuilder => JStringBuilder }
 
 import kse.flow._
 import kse.maths._
@@ -102,12 +102,210 @@ class Summary() {
   }
 }
 object Summary {
-  class Entry private (val t: Double) {
-    def +=(that: Entry): this.type = this
+  final def r1(x: Double): Double =
+    if (x < 0) { if (x < -1e14) x else (x*10 - 0.5).toLong/1e1 }
+    else       { if (x >  1e14) x else (x*10 + 0.5).toLong/1e1 }
+  final def r2(x: Double): Double = 
+    if (x < 0) { if (x < -1e13) x else (x*100 - 0.5).toLong/1e2 }
+    else       { if (x >  1e13) x else (x*100 + 0.5).toLong/1e2 }
+  final def r3(x: Double): Double = 
+    if (x < 0) { if (x < -1e12) x else (x*1000 - 0.5).toLong/1e3 }
+    else       { if (x >  1e12) x else (x*1000 + 0.5).toLong/1e3 }
+  final def r4(x: Double): Double = 
+    if (x < 0) { if (x < -1e11) x else (x*10000 - 0.5).toLong/1e4 }
+    else       { if (x >  1e11) x else (x*10000 + 0.5).toLong/1e4 }
+
+  class Entry private (val t: Double, val jtx: Double = 0.0, val jty: Double = 0.0, val jnx: Int = 0, val jny: Int = 0) {
+    var n = 0
+    var goodN = 0
+    var persist = 0.0
+    var speed = 0.0
+    var angSpeed = 0.0
+    var length = 0.0
+    var relLength = 0.0
+    var width = 0.0
+    var relWidth = 0.0
+    var aspect = 0.0
+    var relAspect = 0.0
+    var wiggle = 0.0
+    var pixels = 0.0
+    var stimString = ""
+    var findLoss: Array[Int] = null
+    var findLossN = 0
+    def +=(that: Entry): this.type = {
+      n += that.n
+      if (that.goodN > 0) {
+        if (goodN == 0) {
+          persist   = that.persist
+          speed     = that.speed
+          angSpeed  = that.angSpeed
+          length    = that.length
+          relLength = that.relLength
+          aspect    = that.aspect
+          relAspect = that.relAspect
+          wiggle    = that.wiggle
+          pixels    = that.pixels
+          goodN = that.goodN
+        }
+        else {
+          val good = (goodN + that.goodN).toDouble
+          persist   = (persist*goodN   + that.persist*that.goodN  ) / good
+          speed     = (speed*goodN     + that.speed*that.goodN    ) / good
+          angSpeed  = (angSpeed*goodN  + that.angSpeed*that.goodN ) / good
+          length    = (length*goodN    + that.length*that.goodN   ) / good
+          relLength = (relLength*goodN + that.relLength*that.goodN) / good
+          width     = (width*goodN     + that.width*that.goodN    ) / good
+          relWidth  = (relWidth*goodN  + that.relWidth*that.goodN ) / good
+          aspect    = (aspect*goodN    + that.aspect*that.goodN   ) / good
+          relAspect = (relAspect*goodN + that.relAspect*that.goodN) / good
+          wiggle    = (wiggle*goodN    + that.wiggle*that.goodN   ) / good
+          pixels    = (pixels*goodN    + that.pixels*that.goodN   ) / good
+          goodN += that.goodN
+        }
+      }
+      if (!that.stimString.isEmpty) {
+        stimString =
+          if (stimString.isEmpty) that.stimString
+          else Entry.bitsToStimString(Entry.stimStringToBits(stimString) | Entry.stimStringToBits(that.stimString))        
+      }
+      if (that.findLossN > 0) {
+        if (findLoss eq null) findLoss = java.util.Arrays.copyOf(that.findLoss, that.findLossN)
+        else if (findLossN + that.findLossN <= findLoss.length) System.arraycopy(findLoss, findLossN, that.findLoss, 0, that.findLossN)
+        else {
+          findLoss = java.util.Arrays.copyOf(findLoss, math.max(findLossN + findLossN>>1 + 2, findLossN + that.findLossN))
+          System.arraycopy(findLoss, findLossN, that.findLoss, 0, that.findLossN)
+        }
+        findLossN += that.findLossN
+      }
+      this
+    }
+    private[this] def addStimString(sb: JStringBuilder, prefix: String): JStringBuilder = 
+      if (stimString.isEmpty) sb
+      else sb append prefix append stimString
+    private[this] def addFindLossString(sb: JStringBuilder, prefix: String): JStringBuilder = 
+      if (findLossN < 2) sb
+      else {
+        sb append prefix
+        var i = 0
+        while (i+3 < findLossN) {
+          sb append findLoss(i) append ' ' append findLoss(i+1) append ' '
+          i += 2
+        }
+        sb append findLoss(i) append ' ' append findLoss(i+1)
+      }
+    private[this] def addPositionString(sb: JStringBuilder, prefix: String): JStringBuilder =
+      if (jtx == 0 && jty == 0) sb
+      else sb append prefix append r3(jtx) append ' ' append r3(jty) append "  " append jnx append ' ' append jny
+    def addString(sb: JStringBuilder): JStringBuilder = {
+      sb.ensureCapacity(100)
+      sb append r3(t)
+      sb append "  " append n append ' ' append goodN append ' ' append r1(persist)
+      sb append "  " append r2(speed) append ' ' append r3(angSpeed)
+      sb append "  " append r1(length) append ' ' append r3(relLength)
+      sb append "  " append r1(width) append ' ' append r3(relWidth)
+      sb append "  " append r3(aspect) append ' ' append r3(relAspect)
+      sb append "  " append r3(wiggle) append ' ' append r3(pixels)
+      addStimString(sb, "  % ")
+      addFindLossString(sb, "  %% ")
+      addPositionString(sb, "  @ ")
+      sb
+    }
+    override def toString = addString(new JStringBuilder).toString
+    override def equals(that: Any) = that match {
+      case e: Entry =>
+        t == e.t &&
+        n == e.n && goodN == e.goodN && persist == e.persist &&
+        speed == e.speed && angSpeed == e.angSpeed &&
+        length == e.length && relLength == e.relLength &&
+        width == e.width && relWidth == e.relWidth &&
+        aspect == e.aspect && relAspect == e.relAspect &&
+        wiggle == e.wiggle && pixels == e.pixels &&
+        (
+          stimString.isEmpty == e.stimString.isEmpty &&
+          (if (stimString.nonEmpty) Entry.stimStringToBits(stimString) == Entry.stimStringToBits(e.stimString) else true)
+        ) &&
+        (
+          findLossN == e.findLossN &&
+          { var i = 0; while (i < findLossN && findLoss(i) == e.findLoss(i)) {}; i == findLossN }
+        ) &&
+        jtx == e.jtx && jty == e.jty && jnx == e.jnx && jny == e.jny
+      case _ => false
+    }
   }
   object Entry {
-    def apply(t: Double) = 
+    def apply(t: Double): Entry = 
       if (t.finite) new Entry(t)
       else throw new IllegalArgumentException(s"Only finite times allowed in summary, not $t")
+    def apply(t: Double, jtx: Double, jty: Double, jnx: Int, jny: Int): Entry =
+      if (t.finite && jtx.finite && jty.finite) new Entry(t, jtx, jty, jnx, jny)
+      else throw new IllegalArgumentException(s"Summary must have finite parameters; found t=$t, jtx=$jtx, jty=$jty")
+    def parse(g: Grok)(implicit fail: GrokHop[g.type]): Entry = {
+      g.delimit(true, 0)
+      val index = g.I 
+      val t = g.D
+      val n = g.I
+      val goodN = g.I
+      val persist = g.D
+      val speed = g.D
+      val angSpeed = g.D
+      val length = g.D
+      val relLength = g.D
+      val width = g.D
+      val relWidth = g.D
+      val aspect = g.D
+      val relAspect = g.D
+      val wiggle = g.D
+      val pixels = g.D
+      var x = g.peekTok
+      val stimString =
+        if ((x ne null) && x == "%") {
+          g.skip
+          val sb = new JStringBuilder
+          while ({x = g.peekTok; (x ne null) && !x.startsWith("%") && !x.startsWith("@")}) {
+            g.skip
+            if (sb.length > 0) sb append ' '
+            sb append x
+          }
+          sb.toString
+        }
+        else ""
+      val findLoss =
+        if ((x ne null) && x == "%%") {
+          g.skip
+          val ab = Array.newBuilder[Int]
+          while ({x = g.peekTok; (x ne null) && !x.startsWith("%") && !x.startsWith("@")}) {
+            ab += g.I
+            ab += g.I
+          }
+          ab.result
+        }
+        else null
+      while (g.hasContent && g.tok != "@") {}
+      val entry =
+        if (g.hasContent) apply(t, g.D, g.D, g.I, g.I)
+        else apply(t)
+      entry.n = n
+      entry.goodN = goodN
+      entry.persist = persist
+      entry.speed = speed
+      entry.angSpeed = angSpeed
+      entry.length = length
+      entry.relLength = relLength
+      entry.width = width
+      entry.relWidth = relWidth
+      entry.aspect = relAspect
+      entry.wiggle = wiggle
+      entry.pixels = pixels
+      if (stimString.nonEmpty) entry.stimString = stimString
+      if (findLoss ne null) {
+        entry.findLoss = findLoss
+        entry.findLossN = findLoss.length
+      }
+      entry
+    }
+    def stimStringToBits(s: String): Long =
+      if (s.startsWith("0x")) java.lang.Long.parseLong(s.substring(2), 16)
+      else s.split("\\s+").map(x => 1L << (x.toInt-1).toLong).reduce(_ | _)
+    def bitsToStimString(l: Long): String = "0x" + l.toHexString
   }
 }
